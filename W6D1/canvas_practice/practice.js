9d9c46d8588dc181c6cd3acff766e97006adc13b
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2018-11-25 00:49:40 -0800

    Add rectangle and circle to HTML canvas

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const canvasEl = document.getElementById("mycanvas");
+    canvasEl.width = 500;
+    canvasEl.height = 500;
 
+    const ctx = canvasEl.getContext("2d");
+
+    //Drawing a rectange.
+    ctx.fillStyle = "rgb(230, 160, 40)";
+    ctx.fillRect(10, 10, 400, 200);
+
+    //Drawing a circle.
+    ctx.beginPath();
+    ctx.arc(200,250, 30, 0, 2*Math.PI);
+    ctx.strokeStyle = 'rgb(180, 66, 222)';
+    ctx.linewidth = 5;
+    ctx.stroke();
+    ctx.fillstyle = "blue";
+    ctx.fill();
+    
+    
+    
+    
 });
